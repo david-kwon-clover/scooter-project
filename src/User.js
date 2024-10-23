0bf62ecdb56017abb67aef09606759186af6ddd8
David Kwon <david.kwon@clover.com>
2024-10-23 12:49:44 -0600

    Implement User class in User.js

--- src/User.js
+++ src/User.js
@@ -1,3 +1,21 @@
-class User {}
+class User {
+    constructor(username, password, age) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.loggedIn = false;
+    }
+
+    login(password) {
+        if(password !== this.password) {
+            throw new Error("incorrect password");
+        }
+        this.loggedIn = true;
+    }
+
+    logout() {
+        this.loggedIn = false;
+    }
+}
 
 module.exports = User;
